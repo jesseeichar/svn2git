4fd3845264c6f1d75bbe291bf6daf29666c24ca2
jeichar <jesse.eichar@camptocamp.com>
2011-02-07 14:10:53 +0100

    changed version number

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,6 +2,5 @@ import sbt._
 import com.github.retronym.OneJarProject
 
 class HelloWorldProject(info: ProjectInfo) extends DefaultProject(info) with OneJarProject
-{
   override def mainClass = Some("Svn2Git")
 }
